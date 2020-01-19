31a604a21a444983045268b99831222c5c8b4b84
Joris Demandre <joris.demandre@agence-soon.fr>
2020-01-19 22:56:56 +0100

    init CS project

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using static cleancode.App;
+
+namespace cleancode
+{
+   class Program
+    {
+        static void Main(string[] args)
+        {
+            cleancode.App app = new cleancode.App();
+            double truc = app.Compute("1","1","1","1","1","1","1");
+            Console.WriteLine(truc);
+        }
+    }
+}
